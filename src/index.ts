3942510868d8700c71a99c6289b36890cacda6d2
Eremina Daria <daria.eremina89@gmail.com>
2026-09-18 15:09:09 +0700

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,10 @@ export { Skeleton } from './components/Skeleton/Skeleton';
 export type { SkeletonProps, SkeletonShape } from './components/Skeleton/Skeleton';
 export { Input } from './components/Input/Input';
 export type { InputProps, InputSize } from './components/Input/Input';
+export { ButtonIcon } from './components/ButtonIcon/ButtonIcon';
+export type {
+  ButtonIconColor,
+  ButtonIconProps,
+  ButtonIconSize,
+  ButtonIconState,
+} from './components/ButtonIcon/ButtonIcon';
